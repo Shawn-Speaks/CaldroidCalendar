f59e9a59285063c29168debe57925b0ee7e67181
shawn-speaks <sspeaks121@gmail.com>
2016-11-16 12:34:52 -0500

    First commit

---
+++ app/src/main/java/shawn/c4q/nyc/caldroidpractice/FragmentActivity.java
@@ -0,0 +1,32 @@
+package shawn.c4q.nyc.caldroidpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+
+        android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.activity_main, caldroidFragment);
+        t.commit();
+
+
+    }
+
+}
